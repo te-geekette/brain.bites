1cc9113921ede28fc878448a43fdf6fda709b307
Tina Egolf <tinaegolf@gmail.com>
2015-12-17 12:46:18 +0100

    Theoretical fix for displaying material-icons on Android. No luck so far.

--- client/subscriptions.js
+++ client/subscriptions.js
@@ -1,12 +1,12 @@
-	Meteor.subscribe('courses');
-	Meteor.subscribe('contentItems');
-	Meteor.subscribe('userData'); 
+Meteor.subscribe('courses');
+Meteor.subscribe('contentItems');
+Meteor.subscribe('userData'); 
 
-	Accounts.onResetPasswordLink(function(token, done){
-		FlowRouter.go('/reset-password/' + token); 
-		// done();
-	});
+Accounts.onResetPasswordLink(function(token, done){
+	FlowRouter.go('/reset-password/' + token); 
+	// done();
+});
 
-	Meteor.startup(function () {
-		FlowRouter.initialize();
-	});
+Meteor.startup(function () {
+	FlowRouter.initialize();
+});
